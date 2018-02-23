dc6390984e7f9f3c352261285851d6306b0ef5c6
Anastasiia_Aloshyna <Anastasiia_Aloshyna@EPUAKYIW0870>
2018-02-23 11:52:20 +0200

    Program cs. was added

---
+++ Count Real Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortWordsSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var part = Console.ReadLine().ToLower().Split('.', ',', ':', ';', '(', ')', '[', ']', '“', '‘', '\\', '/', '!', '?', ' ').Distinct().Where(i => i.Length < 5);
+            IEnumerable<string> word = from p in part orderby p select p;
+            foreach (string i in word)
+            {
+                Console.WriteLine(i + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
