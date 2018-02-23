8b150adde3bff85c0f4c4653041816af1103041b
Anastasiia_Aloshyna <Anastasiia_Aloshyna@EPUAKYIW0870>
2018-02-23 13:56:05 +0200

    Add_RealTask_Count_Real_Number

---
+++ RealTask_Count Real Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTask_Count_Real_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> array = new List<double>() { 1, 1.2, 5, 5, 2.3, 1 };
+            array.Sort();
+
+            foreach (double val in array.Distinct())
+            {
+                Console.WriteLine(val + " - " + array.Where(x => x == val).Count() + " times");
+            }
+            Console.ReadKey();
+        }
+    }
+}
