7ab71ca012aac8256c62f61d7884b107fc23dc25
Anastasiia_Aloshyna <Anastasiia_Aloshyna@EPUAKYIW0870>
2018-02-26 13:29:40 +0200

    Added Task "Int_50_problem"

---
+++ Int_50_problem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Int_50_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Count(); i++)
+            {
+                arr[i] = rnd.Next(-10, 100);
+                Console.Write(arr[i] + " ");
+
+            }
+            IEnumerable<int> arrList = arr.OrderBy(i => i);
+            foreach (int i in arrList)
+            {
+                Console.WriteLine(i + " ");
+            }
+            int number = (from t in arrList where (t == 50) select t).Count();
+            Console.WriteLine("This List has  " + number + " number 50");
+            Console.ReadKey();
+
+        }
+    }
+}
--- Largest3numbers/Program.cs
+++ Largest3numbers/Program.cs
@@ -14,12 +14,12 @@ namespace Largest3numbers
             Random r = new Random();
             for (int i = 0; i < arr.Count(); i++)
             {
-                arr[i] = r.Next(-10, 10);
+                arr[i] = r.Next(0, 10);
                 Console.Write(arr[i] + " ");
 
             }
 
-            var arrList = arr.OrderByDescending(t => t).Take(3).ToList();
+            var arrList = arr.Where(t=>t).OrderByDescending(t => t).Take(3).ToList();
             foreach (int i in arr)
                 Console.WriteLine(i);
 
