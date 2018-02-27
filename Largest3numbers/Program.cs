6422e6af70e1411bed0adef31df9f285d7d1d4ad
Anastasiia_Aloshyna <Anastasiia_Aloshyna@EPUAKYIW0870>
2018-02-27 12:48:49 +0200

    Changes to Largest3numbers""

--- Largest3numbers/Program.cs
+++ Largest3numbers/Program.cs
@@ -13,15 +13,12 @@ namespace Largest3numbers
            
             List<int> arr = new List<int>();
             Random r = new Random();
-            //int[] arr = new int[10];
-
+           
             for (int i = 0; i < 20; i++)
             {
                 arr.Add(r.Next(0, 10));
-                //arr[i] = r.Next(0, 10);
                 Console.Write("{0} ", arr[i]);
-                //Console.Write(arr[i] + " ");
-
+                
             }
 
             var biggerThree = arr.OrderByDescending(t => t).Take(3).ToList();
