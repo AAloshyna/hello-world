2061a4f801ae564e584d7f2bb9cf66ceb79247f0
Anastasiia_Aloshyna <Anastasiia_Aloshyna@EPUAKYIW0870>
2018-03-01 12:27:19 +0200

    Add changres to "Invoice"

--- Invoice/Program.cs
+++ Invoice/Program.cs
@@ -13,29 +13,28 @@ namespace Invoice
         {
 
             var list = new List<Invoice>()
-            {
-                new Invoice { PartNumber = 1, PartDescription = "part7", Quantity = 5, Price = 10.5m },
-                new Invoice { PartNumber = 2, PartDescription = "part2", Quantity = 2, Price = 12.5m },
-                new Invoice { PartNumber = 3, PartDescription = "part5", Quantity = 3, Price = 20.5m },
-                new Invoice { PartNumber = 4, PartDescription = "part4", Quantity = 4, Price = 2.5m }
-            };
+                {
+                    new Invoice { PartNumber = 1, PartDescription = "part7", Quantity = 5, Price = 10.5m },
+                    new Invoice { PartNumber = 2, PartDescription = "part2", Quantity = 2, Price = 12.5m },
+                    new Invoice { PartNumber = 3, PartDescription = "part5", Quantity = 3, Price = 20.5m },
+                    new Invoice { PartNumber = 4, PartDescription = "part4", Quantity = 4, Price = 2.5m }
+                };
+
             Console.WriteLine("Collection: ");
             foreach (var o in list)
             {
                 Console.WriteLine("PartNumber:" + " " + o.PartNumber + " " + "PartDescription:" + " " + o.PartDescription + " " + "Quantity:" + " " + o.Quantity + " " + "Price:" + " " + o.Price);
             }
 
-            Console.WriteLine(    );
-            Console.Write("Sorted by PartDescription: ");
+            Console.Write("\nSorted by PartDescription: ");
             var sorPD = list.OrderBy(q => q.PartDescription).ToList();
 
             foreach (var o in sorPD)
             {
                 Console.Write(o.PartDescription + "  ");
             }
-
-            Console.WriteLine(      );
-            Console.Write("Sorted by Price: ");
+   
+            Console.Write("\nSorted by Price: ");
 
             var sorP = list.OrderBy(q => q.Price).ToList();
             foreach (var o in sorP)
@@ -43,8 +42,7 @@ namespace Invoice
                 Console.Write(o.Price + "  ");
             }
 
-            Console.WriteLine(     );
-            Console.WriteLine("Selecting by PartDescription and Quantity and sort by Quantity: ");
+            Console.WriteLine("\nSelecting by PartDescription and Quantity and sort by Quantity: ");
 
             var pDP = list.Select(q => new { PartDescription = q.PartDescription, Quantity = q.Quantity }).OrderBy(t => t.Quantity).ToList();
 
@@ -53,7 +51,28 @@ namespace Invoice
                 Console.WriteLine("PartDescription:" + " " + o.PartDescription + " " + "Quantity:" + " " + o.Quantity + " ");
             }
 
+            Console.WriteLine("\nSelecting by PartDescription and InvoiceTotal: ");
+            var invTotal = list.Select(q => new { PartDescription = q.PartDescription, invTotal = q.Quantity * q.Price }).ToList();
+            foreach (var o in invTotal)
+            {
+                Console.WriteLine("PartDescription:" + " " + o.PartDescription + " " + "invTotal:" + " " + o.invTotal + " ");
+            }
+
+            Console.WriteLine("\nInvoiceTotal in the range 5-33: ");
+
+            var invTotalInRange = from t in invTotal 
+                                  where t.invTotal > 5 && t.invTotal <30
+                                  //orderby t
+                                  select t;
+
+            foreach (var s in invTotalInRange)
+            {
+                Console.WriteLine("invTotal: " + " " + s.invTotal);
+            }
+               
             Console.ReadKey();
+
+
         }
         class Invoice
         {
